887931d419b6eadd2e1d1250ca04bf6824c904f5
iiwake01 <50771492+iiwake01@users.noreply.github.com>
2023-01-18 23:31:39 +0800

    added setLikes method for likes_builder ongoing

--- lib/widgets/likes_widget.dart
+++ lib/widgets/likes_widget.dart
@@ -1,15 +1,19 @@
+import 'package:cafeterianhs/controllers/main_controller.dart';
 import 'package:cafeterianhs/widgets/base_widgets.dart';
+import 'package:cafeterianhs/widgets/card_view_widget.dart';
+import 'package:cafeterianhs/widgets/sliver_grid_delegate_with_fixed_cross_axis_count_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 
-class LikesWidget extends BaseWidget {
+import '../utils/shop_category_enum.dart';
+
+class LikesWidget extends BaseWidget<MainController> {
   const LikesWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return CustomScrollView(
       slivers: <Widget>[
-        //Appbar
-
         SliverAppBar(
           leading: IconButton(
             onPressed: () {},
@@ -21,6 +25,24 @@ class LikesWidget extends BaseWidget {
             centerTitle: true,
             title: Text('Likes'),
           ),
+        ),
+        Obx(
+          () {
+            return GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCountWidget(),
+              itemCount: controller.getFavoritesLength(),
+              itemBuilder: (_, index) {
+                return CardViewWidget(
+                  image: controller.getFavoritesImage(index),
+                  name: controller.getFavoritesName(index),
+                  price: controller.getFavoritesPrice(index),
+                  onPressedLikes: () {
+                    controller.setLikes(index, ShopCategoryEnum.all);
+                  },
+                );
+              },
+            );
+          },
         ),
       ],
     );
